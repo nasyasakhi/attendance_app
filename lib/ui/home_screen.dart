7ec1fbd530c14d3eed65834ecbb261e6b7b95910
nasyasakhi <nasyasakhi6@gmail.com>
2025-01-21 08:47:54 +0700

    configuring home_screen.dart file

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -21,39 +21,28 @@ class HomeScreen extends StatelessWidget {
         body: SafeArea(
           child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  margin: const EdgeInsets.all(10) ,
-                  child: Expanded(
-                    child: InkWell( // 
-                      highlightColor: Colors.transparent,
-                      splashColor: Colors.transparent,
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => const AttendanceScreen()
-                        )
-                        );
-                      },
-                      child: const Column(
-                       children: [
-                        Image(image: AssetImage('assets/images/ic_attend.png'),
-                        height: 100,
-                        width: 100,
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          "Attendance Report",
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold
-                          ),
-                        )
-                       ],
-                      ),
-                    ),
+                _buidlMenuItem( // ada yang membuat parameter wajib berurutan apa itu?
+                  context,
+                  imagePath: "/assets/images/ic_attend.png",
+                  label: "Attendance Report",
+                  destination: const AttendanceScreen()
                   ),
-                ),
-                
-                SizedBox(height: 40)
+                const SizedBox(height: 40),
+                _buidlMenuItem( 
+                  context,
+                  imagePath: "/assets/images/ic_permission.png",
+                  label: "Permission Report",
+                  destination: const AttendanceScreen()
+                  ),
+                const SizedBox(height: 40),
+                 _buidlMenuItem( 
+                  context,
+                  imagePath: "/assets/images/ic_attendance_history.png",
+                  label: "Attendance History",
+                  destination: const AttendanceScreen()
+                  ),
               ],
             ),
           ),
@@ -61,6 +50,43 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buidlMenuItem(
+    BuildContext context, {
+      required String imagePath,
+      required String label,
+      required Widget destination // pendefinisian class 
+    }) {
+    return Container(
+            margin: const EdgeInsets.all(10) ,
+            child: Expanded(
+               child: InkWell( // efek sentuhan
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => destination));
+                    },
+                    child: Column(
+                     children: [
+                      Image(
+                      image: AssetImage(imagePath),
+                      height: 100,
+                      width: 100,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Attendance Report",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                     ],
+                    ),
+                  ),
+                ),
+              );
+            }
 
   Future<bool> _onWillPop(BuildContext context) async {
     return (await showDialog(
@@ -105,8 +131,10 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       )
-
       )
-    );
+    /* ini adalah default value ketika semua kode yang ada 
+    di blok Alert Dialog tidak tereksekusi karena beberapa hal */
+    ) ??
+    false;
   }
 }
